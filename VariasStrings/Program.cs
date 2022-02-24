3cfed475173cfc40973d49a03f090626349f2c45
Manuel Geraldes <45165444+Nel000@users.noreply.github.com>
2022-02-24 14:36:55 +0000

    Add multiple strings and print them

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string s = "coiso";
+            string t = "coisa";
+            string r = @"""coiso entre aspas""";
+            string i = @"\coiso sem aspas\";
+            string n = "\u00A9 \u2665";
+            string g = @"\u0010 not UTF";
+
+            Console.WriteLine(s);
+            Console.WriteLine(t);
+            Console.WriteLine(r);
+            Console.WriteLine(i);
+            Console.WriteLine(n);
+            Console.WriteLine(g);
+            
+        }
+    }
+}
