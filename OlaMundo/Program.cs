5333450c8ac6a63bb4d40c912fe6c4fae938e567
Manuel Geraldes <45165444+Nel000@users.noreply.github.com>
2022-02-24 11:09:13 +0000

    Add XML documentation

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,8 +2,15 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// This program prints 2 messages: "Hello World!" and "Bye World!".
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// The main method is responsible for printing both messages.
+        /// </summary>
+        /// <param name="args">The arguments given in the command line.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
