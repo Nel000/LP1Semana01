db4dba5e37359c67fb0bbe4bf2ef04a34d269b0f
Manuel Geraldes <45165444+Nel000@users.noreply.github.com>
2022-02-24 14:21:38 +0000

    Add char variables and print them

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace TiposVarios
 {
@@ -6,13 +7,23 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
             int i = 5;
             uint ui = 7U;
             long l = 142L;
 
+            char copyrightSymbol = '\u00A9';
+            char heart = '\u2665';
+
             Console.WriteLine(i);
             Console.WriteLine(ui);
             Console.WriteLine(l);
+
+            Console.WriteLine();
+
+            Console.WriteLine(copyrightSymbol);
+            Console.WriteLine(heart);
         }
     }
 }
