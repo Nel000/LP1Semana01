425c9f02e11664fbe923b6cce508df0c1996a27a
Manuel Geraldes <45165444+Nel000@users.noreply.github.com>
2022-02-24 14:13:36 +0000

    Add integral variables and print them

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            uint ui = 7U;
+            long l = 142L;
+
+            Console.WriteLine(i);
+            Console.WriteLine(ui);
+            Console.WriteLine(l);
+        }
+    }
+}
