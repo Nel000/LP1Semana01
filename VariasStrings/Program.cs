efcc1df996b34425210abd7526181052b729d22d
Manuel Geraldes <45165444+Nel000@users.noreply.github.com>
2022-02-24 14:46:07 +0000

    Add concatenated and interpolated strings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -9,6 +9,9 @@ namespace VariasStrings
         {
             Console.OutputEncoding = Encoding.UTF8;
 
+            int x = 10;
+            char c = 'a';
+
             string s = "coiso";
             string t = "coisa";
             string r = @"""coiso entre aspas""";
@@ -16,13 +19,33 @@ namespace VariasStrings
             string n = "\u00A9 \u2665";
             string g = @"\u0010 not UTF";
 
+            string cs = "Coiso" + "Coisa" + 2;
+            string cr = s + t + x;
+
+            string si = $"{s} interpolado";
+            string ri = $@"{s} + {c} = {t} verbatim {x}";
+
+            string sf = String.Format("{0} e {1}", s, t);
+
             Console.WriteLine(s);
             Console.WriteLine(t);
             Console.WriteLine(r);
             Console.WriteLine(i);
             Console.WriteLine(n);
             Console.WriteLine(g);
+
+            Console.WriteLine();
+
+            Console.WriteLine(cs);
+            Console.WriteLine(cr);
             
+            Console.WriteLine(si);
+            Console.WriteLine(ri);
+
+            Console.WriteLine();
+
+            Console.WriteLine(sf);
+            Console.WriteLine();
         }
     }
 }
